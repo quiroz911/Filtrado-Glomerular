308d193e9cffd12278bf0319f0ae5780766d2c9c
quiroz911 <jpablo.quiroz@udea.edu.co>
2021-01-09 14:53:17 -0500

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filtrado_Glomerular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcularButton_Click(object sender, EventArgs e)
+        {
+            resultadoTxt.Text = edadTxt.Text + pesoTxt.Text;
+        }
+
+        private void limpiarButton_Click(object sender, EventArgs e)
+        {
+            edadTxt.ResetText();
+            pesoTxt.ResetText();
+            creatininaTxt.ResetText();
+            relacionACTxt.ResetText();
+            razaNegraCheckBox.Checked = false;
+            resultadoTxt.ResetText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
